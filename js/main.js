c2ff2ac322ac80b5bd9eeb1eb6478ca52e78404f
birchy009 <birchy009@hotmail.com>
2014-10-24 21:13:23 -0400

    added previously used code

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var $win = $(window);
+var $bgSection = $('.bg-section');
+var $logo = $('.logo');
+var fadeStart=100 // 100px scroll or less will equiv to 1 opacity
+    ,fadeUntil=1000 // 1000px scroll or more will equiv to 0 opacity
+    ,fading = $('.fading')
+;
+
+$(window).bind('scroll', function(){
+    var offset = $(document).scrollTop()
+        ,opacity=0
+    ;
+    if( offset<=fadeStart ){
+        opacity=1;
+    }else if( offset<=fadeUntil ){
+        opacity=1-offset/fadeUntil;
+    }
+    fading.css('opacity',opacity);
+});
+
+
+$win.on('scroll', function () {
+	var scrollPos = $win.scrollTop();
+
+	$bgSection.css('background-position', 'center ' + scrollPos/2 + 'px');
+});
